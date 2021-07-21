f59c3e0e9b8dd04d1765e35459f7b0cad51b787a
tomlarose <thomaslarose@gmail.com>
2021-07-21 04:51:06 -0700

    fixed type

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 git clone https://github.com/NerdEgghead/TBC_cat_sim.git /scripts
 pip install -r /scripts/requirements.txt
 chmod -R +x /scripts
 
-pyton /scripts/main.py
+python /scripts/main.py
